9f8894936e4cf570bf2c8bf74da63775efd377e7
Sharmin Vadaliya <139356720+savadaliya@users.noreply.github.com>
2024-12-05 11:10:57 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "sharmin"
+
+// console.log(typeof score);
+// console.group(typeof(score));
+
+ let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = "sharmin"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); // true
+
+// 1 => true; 0 => false
+// "" => false
+// "sharmin" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
