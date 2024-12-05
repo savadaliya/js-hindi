9fc3be4de6333b57fe87b9d0b00931b70fc1def2
Sharmin Vadaliya <139356720+savadaliya@users.noreply.github.com>
2024-12-05 10:45:22 +0000

    variablas

---
+++ 01_basics/01_variablas.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "savadaliya09@gmail.com"
+var acccountPassword = "12345"
+accountCity = "suart"
+accountState;
+
+// accountId = 2  // not allowed
+
+accountEmail = "sa@gmail.com"
+acccountPassword = "21212121"
+accountCity = "saurat"
+
+console.log(accountId)
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, acccountPassword, accountCity, accountState])
