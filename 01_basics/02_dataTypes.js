587ddb37cf76c3fdfb845b51b66c78e1152cf928
Sharmin Vadaliya <139356720+savadaliya@users.noreply.github.com>
2024-12-05 08:33:45 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alect( 3 + 3) // we are using nodejs, not browser
+
+// console.log(3+3) console.log("Sharmin")
+console.log(3+3)
+console.log("Sharmin")
+// code readability should be hight
+
+let name = "sharmin"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// nummber => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => variable is declared but not assigned
+// symbol =>unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
